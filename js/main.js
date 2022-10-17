2c9d1562b44643b4553c91e8de027c1330f5d69c
EyeMLeo <Leonid.Akuta@gmail.com>
2022-10-17 10:18:43 +0200

    1ma uzduotis - jazz, and rock n roll

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 'use strict'
 console.log('main.js');
+
+
+
+let array=["Jazz","Bluez"]
+console.log(array);
+
+array.push("Rock-n-roll");
+console.log(array);
+
+array[1]="Classics";
+console.log(array);
+
+console.log(array.shift(1));
+console.log(array);
+
+array.unshift("Rap","Reggae")
+console.log(array);
+
+
